161a3256b6ff02bc245ca31b14fc1f568d120c05
r-a-m-e-s-h-q-a <ramesh.qa8795@gmail.com>
2024-12-06 16:24:18 +0530

    Initial Commit

---
+++ src/test/java/GitPractice_File1.java
@@ -0,0 +1,31 @@
+
+public class GitPractice_File1 {
+	
+	public void method1()
+	{
+		System.out.println("From method 1");
+		System.out.println("From method 1");
+	}
+	
+	public void method2()
+	{
+		System.out.println("from method 2");
+		System.out.println("from method 2");
+	}
+	
+	public void method3()
+	{
+		System.out.println("from method 3");
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		GitPractice_File1 gp = new GitPractice_File1();
+		gp.method1();
+	
+		
+	}
+	
+
+}
